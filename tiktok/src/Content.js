32cf5d4fa271314671f8f060584abf4ac81b661c
Khánh Duy <duyntk3020@gmail.com>
2024-03-27 22:34:00 +0700

    update

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -11,22 +11,35 @@
 
 import { useState, useEffect } from "react";
 
-const tabs = ["posts", "comments", "albums"];
+const tabs = ["posts", "comments", "albums", "photos", "todos", "users"];
 
 function Content() {
     const [title, setTitle] = useState("");
     const [posts, setPosts] = useState([]);
     const [type, setType] = useState("posts");
+    const [showGoToTop, setShowGoToTop] = useState(false);
 
     useEffect(() => {
         document.title = title;
-        console.log('titlechange')
+        console.log("titlechange");
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then((res) => res.json())
             .then((posts) => {
                 setPosts(posts);
             });
     }, [type]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setShowGoToTop(window.scrollY >= 200);
+        };
+        window.addEventListener("scroll", handleScroll);
+
+        // Clearn Fuction
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <div>
@@ -49,6 +62,11 @@ function Content() {
                     <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
+            {showGoToTop && (
+                <button style={{ position: "fixed", right: 20, bottom: 20 }}>
+                    Go To Top
+                </button>
+            )}
         </div>
     );
 }
