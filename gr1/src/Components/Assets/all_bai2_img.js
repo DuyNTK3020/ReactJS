6150a2d4ebfb5565f9865829c1bfa08ab4a434fa
Khánh Duy <duyntk3020@gmail.com>
2024-03-30 00:28:55 +0700

    update

---
+++ gr1/src/Components/Assets/all_bai2_img.js
@@ -0,0 +1,23 @@
+import bai2_img_1 from "./bai2_img_1.jpg";
+import bai2_img_2 from "./bai2_img_2.jpg";
+import bai2_img_3 from "./bai2_img_3.jpg";
+import bai2_img_4 from "./bai2_img_4.jpg";
+import bai2_img_5 from "./bai2_img_5.jpg";
+import bai2_img_6 from "./bai2_img_6.jpg";
+import bai2_img_7 from "./bai2_img_7.jpg";
+import bai2_img_8 from "./bai2_img_8.jpg";
+import bai2_img_9 from "./bai2_img_9.jpg";
+
+const all_bai2_img = {
+    bai2_img_1,
+    bai2_img_2,
+    bai2_img_3,
+    bai2_img_4,
+    bai2_img_5,
+    bai2_img_6,
+    bai2_img_7,
+    bai2_img_8,
+    bai2_img_9
+};
+
+export default all_bai2_img;
--- gr1/src/Pages/Bai2.jsx
+++ gr1/src/Pages/Bai2.jsx
@@ -1,8 +1,87 @@
-import React from 'react'
-import './Bai2.scss'
+import React, { useState, useEffect } from 'react';
+import './Bai2.scss';
+import all_bai2_img from '../Components/Assets/all_bai2_img.js'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Bai2 = () => {
-  return (
-    <div>Bai2</div>
-  )
-}
+    const [rows, setRows] = useState(3);
+    const [cols, setCols] = useState(3);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        initializeData(rows, cols);
+    }, []);
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const newRows = parseInt(e.target.row.value);
+        const newCols = parseInt(e.target.col.value);
+        setRows(newRows);
+        setCols(newCols);
+        toast.success(`Đã chỉnh sửa lại thành bảng ${newRows}x${newCols}`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+        initializeData(newRows, newCols);
+    };
+
+    const initializeData = (rows, cols) => {
+        const newData = [];
+        for (let i = 0; i < rows; i++) {
+            const row = [];
+            for (let j = 0; j < cols; j++) {
+                row.push('');
+            }
+            newData.push(row);
+        }
+        setData(newData);
+    }; 
+
+    const renderBoard = () => {
+        return (
+            <div className="board">
+                {data.map((row, rowIndex) => (
+                    <div key={rowIndex} className="row">
+                        {row.map((cell, colIndex) => (
+                            <div key={`${rowIndex}-${colIndex}`} className="boxes">
+                                <img src={all_bai2_img[`bai2_img_${rowIndex * cols + colIndex + 1}`]} alt={`Image ${rowIndex * cols + colIndex + 1}`} />
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    return (
+        <div className="Bai2">
+            <h1>Bài 2: Hiển thị ảnh (image) theo lưới (Grid)​</h1>
+            <form onSubmit={handleFormSubmit} className="form">
+                <div className="form-group">
+                    <label htmlFor="name">Hàng</label>
+                    <input type="number" id="row" name="row" required />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="id">Cột</label>
+                    <input type="number" id="col" name="col" required />
+                </div>
+
+                <div className="form-group form-btn">
+                    <button className="btn" type="submit">
+                        Chỉnh sửa
+                    </button>
+                </div>
+            </form>
+            {renderBoard()}
+            <ToastContainer />
+        </div>
+    );
+};
